d6f7b060f8b6a0130f858db5e2dee7967f176b8f
Chao Ma <super11040@gmail.com>
2020-06-04 14:20:08 -0700

    can query both global and local vocabularies.

--- R_Functions/gts.range.R
+++ R_Functions/gts.range.R
@@ -1,32 +1,28 @@
-gts.range = function(geotime1, geotime2, endpoint=, graph=){
+# INPUT: 
+# 1. geotime1: time in geology, e.g. 50 Ma
+# 2. geotime2: time in geology, e.g. 100 Ma
+# 3. prefix: optional, the prefix that need to be added
+# 4. graph: optional, the graph user provided
+
+# OUTPUT:
+# all the geoConcepts that is between geotime1 and geotime2.
+
+gts.range = function(geotime1, geotime2, endpoint=NULL, graph=NULL){
         
-        # endpoint need to update accordingly
+        # set up an endpoint
         endpoint = "http://virtuoso.nkn.uidaho.edu:8890/sparql/"
         
-        sparql_prefix = " prefix tssc: <http://deeptimekb.org/tssc#> 
-                  prefix tsnc: <http://deeptimekb.org/tsnc#> 
-                  prefix dc: <http://purl.org/dc/elements/1.1/> 
-                  prefix dcterms: <http://purl.org/dc/terms/> 
-                  prefix foaf: <http://xmlns.com/foaf/0.1/> 
-                  prefix geo: <http://www.opengis.net/ont/geosparql#> 
-                  prefix gts: <http://resource.geosciml.org/ontology/timescale/gts#> 
-                  prefix isc: <http://resource.geosciml.org/classifier/ics/ischart/> 
-                  prefix owl: <http://www.w3.org/2002/07/owl#> 
-                  prefix rank: <http://resource.geosciml.org/ontology/timescale/rank/> 
-                  prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-                  prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-                  prefix samfl: <http://def.seegrid.csiro.au/ontology/om/sam-lite#> 
-                  prefix sf: <http://www.opengis.net/ont/sf#> 
-                  prefix skos: <http://www.w3.org/2004/02/skos/core#> 
-                  prefix sosa: <http://www.w3.org/ns/sosa/> 
-                  prefix thors: <http://resource.geosciml.org/ontology/timescale/thors#> 
-                  prefix time: <http://www.w3.org/2006/time#> 
-                  prefix ts: <http://resource.geosciml.org/vocabulary/timescale/> 
-                  prefix vann: <http://purl.org/vocab/vann/> 
-                  prefix void: <http://rdfs.org/ns/void#> 
-                  prefix xkos: <http://rdf-vocabulary.ddialliance.org/xkos#> 
-                  prefix xsd: <http://www.w3.org/2001/XMLSchema#> 
-                "
+        # SPARQL prefix
+        sparql_prefix = " 
+                prefix dc: <http://purl.org/dc/elements/1.1/>
+                prefix gts: <http://resource.geosciml.org/ontology/timescale/gts#>
+                prefix skos: <http://www.w3.org/2004/02/skos/core#>
+                prefix time: <http://www.w3.org/2006/time#>
+                prefix ts: <http://resource.geosciml.org/vocabulary/timescale/> 
+                prefix isc: <http://resource.geosciml.org/classifier/ics/ischart/> 
+        "
+        
+        # sort the two input geotimes and find the old one as the begging time and the younger one as the end time.
         if(geotime1 > geotime2){
                 T_beg = geotime1
                 T_end = geotime2
@@ -36,24 +32,45 @@ gts.range = function(geotime1, geotime2, endpoint=, graph=){
                 T_end = geotime1
         }
 
-        q = paste(sparql_prefix, "
-       SELECT   ?aa
+        # select the geotime that is between geotime1 and geotime2
+        q = paste(sparql_prefix, '
+       SELECT   str(?label) AS ?geoConcept
        WHERE
        {
-       GRAPH <http://deeptimekb.org/tswebrbajpau> 
+       GRAPH <http://deeptimekb.org/iscallnew> 
        {
-       ?s rdfs:label ?aa ;
+       {?tconcept  a gts:GeochronologicEra ;  
+                       rdfs:label ?label ;
        time:hasBeginning ?beg ;
        time:hasEnd ?end .
        ?beg time:inTemporalPosition ?begTime .
        ?end time:inTemporalPosition ?endTime .
        ?begTime time:numericPosition ?begTimeValue .
-       FILTER(?begTimeValue <", T_beg, ") .
+       FILTER(?begTimeValue <=', T_beg, ') .
        ?endTime time:numericPosition ?endTimeValue .
-       FILTER(?endTimeValue >", T_end, ") .
+       FILTER(?endTimeValue >=', T_end, ') .}
+       UNION
+                  {
+                             ?tconcept a gts:GeochronologicEra ;  
+                                       rdfs:label ?label .
+                             ?tconcept dc:description
+                             [time:hasBeginning ?beg ;
+                              time:hasEnd ?end ;
+                              skos:inScheme  ts:isc2012-08].
+                              ?beg time:inTemporalPosition ?begTime .
+                              ?end time:inTemporalPosition ?endTime .
+                              ?begTime dc:description
+                              [time:numericPosition ?begTimeValue ;
+                              skos:inScheme  ts:isc2012-08].
+                              ?endTime dc:description
+                              [time:numericPosition ?endTimeValue ;
+                              skos:inScheme  ts:isc2012-08].
+                             FILTER(?begTimeValue <=', T_beg, ') .
+                             FILTER(?endTimeValue >=', T_end, ') .
+                             }
+       
        }
-       }")
-        
+       }')
         res = SPARQL(endpoint, q)$results
         return(res)
 }
--- R_Functions/gts1.R
+++ R_Functions/gts1.R
@@ -1,77 +1,81 @@
 # INPUT: 
-# 1. geotime: geological time concept, eg. Cambrian
-# 2. level: the level of the geological time concept, eg. Period
-# 3. prefix: optional, the prefix that need to be added
-# 4. graph: optional, the graph user provided
+# 1. geoConcept: geological time concept, eg. Cambrian
+# 2. prefix: optional, the prefix that need to be added
+# 3. graph: optional, the graph user provided
 
 # OUTPUT:
 # begin and end time of the geotime and its duration.
 
-gts1 = function(geotime, level, prefix = NULL, graph = NULL){
+gts1 = function(geoConcept, prefix = NULL, graph = NULL){
   
   # set up end point
   endpoint = "http://virtuoso.nkn.uidaho.edu:8890/sparql/"
   
   # attach SPARQL querry prefix. Note: the graph for our study should be updated
-  sparql_prefix = "
-    prefix tssc: <http://deeptimekb.org/tssc#> 
-    prefix tsnc: <http://deeptimekb.org/tsnc#> 
-    prefix tswe: <http://deeptimekb.org/tswe#> 
-    prefix tsbr: <http://deeptimekb.org/tsbr#>
-    prefix tsba: <http://deeptimekb.org/tsba#> 
-    prefix tsjp: <http://deeptimekb.org/tsjp#> 
-    prefix tsau: <http://deeptimekb.org/tsau#>                   
-    prefix tsnc: <http://deeptimekb.org/tsnc#> 
-    prefix dc: <http://purl.org/dc/elements/1.1/> 
-    prefix dcterms: <http://purl.org/dc/terms/> 
-    prefix foaf: <http://xmlns.com/foaf/0.1/> 
-    prefix geo: <http://www.opengis.net/ont/geosparql#> 
-    prefix gts: <http://resource.geosciml.org/ontology/timescale/gts#> 
-    prefix isc: <http://resource.geosciml.org/classifier/ics/ischart/> 
-    prefix owl: <http://www.w3.org/2002/07/owl#> 
-    prefix rank: <http://resource.geosciml.org/ontology/timescale/rank/> 
-    prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
-    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> 
-    prefix samfl: <http://def.seegrid.csiro.au/ontology/om/sam-lite#> 
-    prefix sf: <http://www.opengis.net/ont/sf#> 
-    prefix skos: <http://www.w3.org/2004/02/skos/core#> 
-    prefix sosa: <http://www.w3.org/ns/sosa/> 
-    prefix thors: <http://resource.geosciml.org/ontology/timescale/thors#> 
-    prefix time: <http://www.w3.org/2006/time#> 
-    prefix ts: <http://resource.geosciml.org/vocabulary/timescale/> 
-    prefix vann: <http://purl.org/vocab/vann/> 
-    prefix void: <http://rdfs.org/ns/void#> 
-    prefix xkos: <http://rdf-vocabulary.ddialliance.org/xkos#> 
-    prefix xsd: <http://www.w3.org/2001/XMLSchema#> 
-  "
+  sparql_prefix = " 
+                prefix dc: <http://purl.org/dc/elements/1.1/>
+                prefix gts: <http://resource.geosciml.org/ontology/timescale/gts#>
+                prefix skos: <http://www.w3.org/2004/02/skos/core#>
+                prefix time: <http://www.w3.org/2006/time#>
+                prefix ts: <http://resource.geosciml.org/vocabulary/timescale/> 
+                prefix isc: <http://resource.geosciml.org/classifier/ics/ischart/> 
+        "
   if (!is.null(prefix)){
     sparql_prefix = paste(prefix, sparql_prefix, sep = "/n")
   }
 
   # run the query
-  q = paste0(sparql_prefix, '
-       SELECT   ?begTimeValue ?endTimeValue
-       WHERE
-       {
-       GRAPH <http://deeptimekb.org/tswebrbajpau> 
-       {
-       ?s rdfs:label "', geotime, " ", level, '"@en;
-       time:hasBeginning ?beg ;
-       time:hasEnd ?end .
-       ?beg time:inTemporalPosition ?begTime .
-       ?end time:inTemporalPosition ?endTime .
-       ?begTime time:numericPosition ?begTimeValue .
-       ?endTime time:numericPosition ?endTimeValue .
-       }
-       }')
+  q = paste0(
+              sparql_prefix, 
+                  '
+                   SELECT   ?begTimeValue ?endTimeValue
+                   WHERE
+                   {
+                     GRAPH <http://deeptimekb.org/iscallnew> 
+                     {
+                             {
+                             ?tconcept  a gts:GeochronologicEra ;  
+                                             rdfs:label ?label .
+                             FILTER (lang(?label) = "en")
+                             FILTER strstarts(?label, "', geoConcept, '").
+                             ?tconcept time:hasBeginning ?beg ;
+                             time:hasEnd ?end .
+                             ?beg time:inTemporalPosition ?begTime .
+                             ?end time:inTemporalPosition ?endTime .
+                             ?begTime time:numericPosition ?begTimeValue .
+                             ?endTime time:numericPosition ?endTimeValue .
+                             }
+                       UNION
+                             {
+                             ?tconcept a gts:GeochronologicEra ;  
+                                       rdfs:label ?label .
+                             FILTER (lang(?label) = "en")
+                             FILTER strstarts(?label, "', geoConcept, '").
+                             ?tconcept dc:description
+                             [time:hasBeginning ?beg ;
+                              time:hasEnd ?end ;
+                              skos:inScheme  ts:isc2012-08].
+                              ?beg time:inTemporalPosition ?begTime .
+                              ?end time:inTemporalPosition ?endTime .
+                              ?begTime dc:description
+                              [time:numericPosition ?begTimeValue ;
+                              skos:inScheme  ts:isc2012-08].
+                              ?endTime dc:description
+                              [time:numericPosition ?endTimeValue ;
+                              skos:inScheme  ts:isc2012-08].
+                             }
+                     }
+                   }
+                  '
+              )
   res = SPARQL(endpoint, q)$results
   
   # display the SPARQL query
-  cat("*****************************************\n")
-  cat("#### Running the following query #####\n")
-  cat("*****************************************\n")
-  cat(q)
-  cat("\n*****************************************\n")
+  #cat("*****************************************\n")
+  #cat("#### Running the following query #####\n")
+  #cat("*****************************************\n")
+  #cat(q)
+  #cat("\n*****************************************\n")
   
   # calculate the druation
   res$duration = abs(res[[1]]-res[[2]])
