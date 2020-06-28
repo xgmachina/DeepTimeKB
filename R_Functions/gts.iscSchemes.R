3bcf52f558586f95b47d1ce4e5bd2723acd167de
Chao Ma <super11040@gmail.com>
2020-06-27 17:26:41 -0700

    Update gts.iscSchemes.R

--- R_Functions/gts.iscSchemes.R
+++ R_Functions/gts.iscSchemes.R
@@ -7,7 +7,11 @@
 # OUTPUT:
 # If id is all, return all isc scheme IDs. If id is latest, return the latest isc Scheme ID
 
-gts.iscSchemes = function(id, prefix = NULL, graph = NULL, URI = TRUE){
+gts.iscSchemes = function(id = "all", prefix = NULL, graph = NULL, URI = TRUE){
+  
+  if(id != "all" & id != "latest"){
+    stop('id should be "all" or "latest"')
+  }
   
   # find all regions and version
   res = gts.listRegion(prefix = prefix, graph = graph)
