62bc43c1a63669a5074abc7b45981432cda74e27
Chao Ma <super11040@gmail.com>
2020-06-27 17:26:48 -0700

    Update gts.level.R

--- R_Functions/gts.level.R
+++ R_Functions/gts.level.R
@@ -9,9 +9,11 @@
 gts.level = function(geoConcept, region = NULL, iscVersion = NULL, prefix = NULL, graph = NULL){
   # get gts.range of the geoConcept. The result's first two columns are schemeID and geoConcept with level
   r1 = gts.range(geoConcept = geoConcept, region = region, iscVersion = iscVersion, prefix = prefix, graph = graph)
+  
   # get schemeID and geoConcept with level
   r1 = r1[,1:2]
   r1$level = sub(".* ", "", r1[,2])
   r1$geoConcpet = geoConcept
+  
   return(r1)
 }
